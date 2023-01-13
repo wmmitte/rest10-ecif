9e443789fdac93462208d137576e54b049294d68
Mitte <wmyameogo@songo-technologies.com>
2023-01-13 07:42:19 +0000

    bd configt 2

--- core/api-class/db.php
+++ core/api-class/db.php
@@ -3,7 +3,7 @@
 class DB {
 
     const DB_SERVER = "127.0.0.1";
-    const DB_USER = "sngse";
+    const DB_USER = "sngs";
     //const DB_USER = "root";
     //const DB_PASSWORD = "";
     const DB_PASSWORD = "";
