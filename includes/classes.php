11a754ef56d0233562857b25b4690b3199d025ae
Mitte <wmyameogo@songo-technologies.com>
2023-02-06 20:43:16 +0000

    sortie function 1 getasorties add

--- includes/classes.php
+++ includes/classes.php
@@ -7,5 +7,6 @@ require 'core/reglement.class.php';
 require 'core/decaissement.class.php';
 require 'core/etat.class.php';
 require 'core/stock.class.php';
+require 'core/sortie.class.php';
 require 'core/rechercheData.class.php';
 ?>
