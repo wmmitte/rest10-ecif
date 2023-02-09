e93245e26bcbcb4e5e511e34f621502e3e3571a1
Mitte <wmyameogo@songo-technologies.com>
2023-02-09 10:41:24 +0000

    article insertStoockSortie add

--- index.php
+++ index.php
@@ -161,6 +161,7 @@ $app->post('/queryetatsortie/', 'queryEtatSortie');
 $app->post('/querysorties/', 'querySorties');
 $app->post('/queryarticlesorties/', 'queryArticleSorties');
 $app->post('/insertsortie/', 'insertSortie');
+$app->post('/insertstocksortie/', 'insertStockSortie');
 
 // Articles
 $app->post('/queryextcategoriesofmag/', 'getExtCategoriesOfMag');
@@ -369,6 +370,32 @@ function insertSortie() {
     try {
 
         $result = $StkM->insertSortie($_PObj);
+
+        $response = array("status" => 0,
+            "datas" => $result,
+            "msg" => "");
+        echo json_encode($response);
+    } catch (Exception $e) {
+        $response = array("status" => 1,
+            "error" => $e->getMessage());
+        echo json_encode($response);
+    }
+}
+
+
+function insertStockSortie() {
+    $request = \Slim\Slim::getInstance()->request();
+    $_PObj = json_decode($request->getBody(), true);
+
+    /* Variables */
+
+    /* class instance */
+    $StkM = new sortieModel();
+
+    /* begin */
+    try {
+
+        $result = $StkM->insertStockSort($_PObj);
 
         $response = array("status" => 0,
             "datas" => $result,
